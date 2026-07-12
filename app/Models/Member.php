6b96d32c0e157333efc160c8aeb92e143064dfda
Hiero <hiero.dhp@gmail.com>
2026-07-12 21:35:34 +0700

    feat(auth): add login logic

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -5,12 +5,12 @@ namespace App\Models;
 use Database\Factories\MemberFactory;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 #[Hidden(['password'])]
-class Member extends Model
+class Member extends Authenticatable
 {
     /** @use HasFactory<MemberFactory> */
     use HasFactory;
--- app/Models/Staff.php
+++ app/Models/Staff.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Database\Factories\StaffFactory;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 #[Hidden(['password'])]
-class Staff extends Model
+class Staff extends Authenticatable
 {
     /** @use HasFactory<StaffFactory> */
     use HasFactory;
